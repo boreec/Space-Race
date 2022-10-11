a3dc03678294e69c412cf577f99166d9a5615c4e
Cyprien Borée <boreec@tuta.io>
2022-10-11 17:25:46 +0900

    set tail object for Missile

--- src/entity.rs
+++ src/entity.rs
@@ -11,6 +11,7 @@ use crate::WINDOW_WIDTH;
 
 const MISSILE_HEIGHT: u32 = 5;
 const MISSILE_WIDTH: u32 = 10;
+const MISSILE_TAIL_SIZE: u32 = MISSILE_HEIGHT;
 const MISSILE_BODY_COLOR: Color = Color::WHITE;
 const MISSILE_TAIL_COLOR: Color = Color::GREY;
 const MISSILE_QUANTITY: usize = 10;
@@ -42,6 +43,7 @@ pub struct Missile {
     pub x: i32,
     pub y: i32,
     pub body: MissileBody,
+    pub tail: MissileTail,
 }
 
 pub struct MissileBody {
@@ -64,6 +66,22 @@ impl MissileBody {
     }
 }
 
+impl MissileTail {
+    pub fn new(origin_x: i32, origin_y: i32) -> MissileTail {
+        let p1 = Point::new(origin_x, origin_y);
+        let p2 = Point::new(origin_x, origin_y - MISSILE_TAIL_SIZE as i32);
+        let p3 = Point::new(origin_x + MISSILE_TAIL_SIZE as i32, origin_y);
+        let p4 = Point::new(origin_x, origin_y + MISSILE_HEIGHT as i32);
+        let p5 = Point::new(origin_x, origin_y + (MISSILE_HEIGHT + MISSILE_TAIL_SIZE) as i32);
+        let p6 = Point::new(origin_x + MISSILE_TAIL_SIZE as i32, origin_y + MISSILE_HEIGHT as i32);
+        return MissileTail {
+            top_triangle: [p1, p2, p3],
+            bottom_triangle: [p4, p5, p6],
+            color: MISSILE_TAIL_COLOR,
+        };
+    }
+}
+
 impl Missile {
     pub fn new() -> Missile {
 
@@ -73,11 +91,13 @@ impl Missile {
 
         let r = Rect::new(pos_x, pos_y, MISSILE_WIDTH, MISSILE_HEIGHT);
         let m = MissileBody::new(r);
-        
+        let t = MissileTail::new(pos_x, pos_y);
+
         return Missile {
             x: pos_x,
             y: pos_y,
             body: m,
-        }
+            tail: t,
+        };
     }
 }
