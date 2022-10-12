438a13796e74ac79dccbecabd44436329f5cb9c3
Cyprien Borée <boreec@tuta.io>
2022-10-12 13:38:55 +0900

    add head Struct for Missile

--- src/entity.rs
+++ src/entity.rs
@@ -11,8 +11,10 @@ use crate::WINDOW_WIDTH;
 const MISSILE_HEIGHT: u32 = 5;
 const MISSILE_WIDTH: u32 = 10;
 const MISSILE_TAIL_SIZE: u32 = MISSILE_HEIGHT;
+const MISSILE_HEAD_SIZE: u32 = MISSILE_HEIGHT;
 const MISSILE_BODY_COLOR: Color = Color::WHITE;
 const MISSILE_TAIL_COLOR: Color = Color::GREY;
+const MISSILE_HEAD_COLOR: Color = Color::RED;
 const MISSILE_QUANTITY: usize = 10;
 const MISSILE_SPAWN_RANGE_X: RangeInclusive<u32> = 0..=(WINDOW_WIDTH - MISSILE_WIDTH);
 const MISSILE_SPAWN_RANGE_Y: RangeInclusive<u32> = 0..=(WINDOW_HEIGHT - 200);
@@ -43,6 +45,7 @@ pub struct Missile {
     pub y: i32,
     pub body: MissileBody,
     pub tail: MissileTail,
+    pub head: MissileHead,
 }
 
 pub struct MissileBody {
@@ -55,6 +58,12 @@ pub struct MissileTail {
     pub top_triangle_y: [i16;3],
     pub bot_triangle_x: [i16;3],
     pub bot_triangle_y: [i16;3],
+    pub color: Color,
+}
+
+pub struct MissileHead {
+    pub triangle_x: [i16;3],
+    pub triangle_y: [i16;3],
     pub color: Color,
 }
 
@@ -80,6 +89,16 @@ impl MissileTail {
     }
 }
 
+impl MissileHead {
+    pub fn new(origin_x: i16, origin_y: i16) -> MissileHead {
+        return MissileHead {
+            triangle_x: [origin_x + MISSILE_WIDTH as i16, origin_x + (MISSILE_WIDTH + MISSILE_HEAD_SIZE) as i16, origin_x + MISSILE_WIDTH as i16],
+            triangle_y: [origin_y, origin_y + MISSILE_HEIGHT as i16 / 2, origin_y + MISSILE_HEIGHT as i16],
+            color: MISSILE_HEAD_COLOR,
+        };
+    }
+}
+
 impl Missile {
     pub fn new() -> Missile {
 
@@ -90,12 +109,14 @@ impl Missile {
         let r = Rect::new(pos_x, pos_y, MISSILE_WIDTH, MISSILE_HEIGHT);
         let m = MissileBody::new(r);
         let t = MissileTail::new(pos_x as i16, pos_y as i16);
+        let h = MissileHead::new(pos_x as i16, pos_y as i16);
 
         return Missile {
             x: pos_x,
             y: pos_y,
             body: m,
             tail: t,
+            head: h,
         };
     }
 }
