7c8561f3b97bde398fe9070f5f540a5a45d69ef6
Cyprien Borée <boreec@tuta.io>
2022-10-16 12:55:25 +0900

    move p1 spaceship only if can_move_downward return true

--- src/main.rs
+++ src/main.rs
@@ -84,7 +84,9 @@ fn handle_events(
                 }
             }
             Event::KeyDown { keycode: Some(Keycode::Down), .. } => {
-                gs.spaceship_p1.move_downward();
+                if gs.spaceship_p1.can_move_downward() {
+                    gs.spaceship_p1.move_downward();
+                }
             }
             Event::KeyDown { keycode: Some(Keycode::Space), .. } => {
                 gs.is_game_restarted = true;
