2388f03de5b4cdccf39c12a79899be3992bb46f0
Cyprien Borée <boreec@tuta.io>
2022-10-24 18:25:54 +0900

    add constants and set screen to black for game_over

--- src/game_over.rs
+++ src/game_over.rs
@@ -1,9 +1,17 @@
+use sdl2::pixels::Color;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
 
 use crate::GameState;
 
-pub fn show_game_over(gs: &mut GameState, canvas: &Canvas<Window>) -> bool {
+const VICTORY_TITLE: &str = "VICTORY";
+const DEFEAT_TITLE: &str = "DEFEAT";
+const DRAW_TITLE: &str= "DRAW";
+
+pub fn show_game_over(gs: &mut GameState, canvas: &mut Canvas<Window>) -> bool {
+    canvas.set_draw_color(Color::BLACK);
+    canvas.clear();
+    // to do
     true
 }
 
