fd2dc24347a3b242f01b0264340de832bbea548f
Cyprien Borée <boreec@tuta.io>
2022-10-24 18:06:48 +0900

    add attribute in GameSFX struct for score.mp3

--- src/entity.rs
+++ src/entity.rs
@@ -22,6 +22,7 @@ pub struct GameState {
 pub struct GameSFX {
     soloud: Soloud,
     collision_wav: Wav,
+    score_wav: Wav,
 }
 
 impl GameState {
@@ -79,9 +80,11 @@ impl GameSFX {
         let mut sounds = GameSFX {
             soloud: sl,
             collision_wav: audio::Wav::default(),
+            score_wav: audio::Wav::default(),
         };
 
         let sfx_collision_path: &Path = std::path::Path::new("asset/sfx/pew.wav");
+        let sfx_score_path: &Path = std::path::Path::new("asset/sfx/score.mp3");
         sounds
             .collision_wav
             .load(sfx_collision_path)
@@ -89,6 +92,15 @@ impl GameSFX {
                 panic!(
                     "failed to load sfx file {} for collision",
                     sfx_collision_path.display()
+                )
+            });
+        sounds
+            .score_wav
+            .load(sfx_score_path)
+            .unwrap_or_else(|_| {
+                panic!(
+                    "failed to load sfx file {} for score",
+                    sfx_score_path.display()
                 )
             });
         sounds
