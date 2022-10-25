2030724f72c3ce464a126a2b9fccde062a8818c7
Cyprien Borée <boreec@tuta.io>
2022-10-25 14:42:28 +0900

    store poetsen font path in GameFont and add method get_font

--- src/game_font.rs
+++ src/game_font.rs
@@ -1,13 +1,29 @@
 use sdl2::ttf::*;
 
-pub struct GameFont {
+use std::path::Path;
+
+pub struct GameFont<'a> {
     pub context: Sdl2TtfContext,
+    poetsen_path: &'a Path,
 }
 
-impl GameFont {
-    pub fn new() -> GameFont {
+impl GameFont<'static> {
+    pub fn new() -> GameFont<'static> {
+
+        let ct = sdl2::ttf::init().
+            unwrap_or_else(|_| panic!("Failed to initialize SDL_TTF!"));
+    
         GameFont {
-            context: sdl2::ttf::init().unwrap_or_else(|_| panic!("Failed to initialize SDL TTF")),
+            context: ct,
+            poetsen_path: Path::new("asset/font/poetsen_one/PoetsenOne-Regular.ttf"),
         }
     }
+
+    pub fn get_font(&self, path: &Path, point_size: u16) -> Font {
+        self.context
+            .load_font(path, point_size)
+            .unwrap_or_else(|_| panic!("Failed to load font {}", path.display()))
+    }
+
+    
 }
