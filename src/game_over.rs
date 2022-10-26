02a24ccf7297706dc9828849dd43b0eff5a96171
Cyprien Borée <boreec@tuta.io>
2022-10-26 19:58:37 +0900

    set the title of game over screen depending on the game score

--- src/game_over.rs
+++ src/game_over.rs
@@ -1,15 +1,22 @@
 use sdl2::pixels::Color;
+use sdl2::rect::Rect;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
 
+use std::cmp::Ordering;
 use std::path::Path;
 
 use crate::GameFont;
 use crate::GameState;
+use crate::WINDOW_HEIGHT;
+use crate::WINDOW_WIDTH;
 
 const VICTORY_TITLE: &str = "VICTORY";
 const DEFEAT_TITLE: &str = "DEFEAT";
 const DRAW_TITLE: &str= "DRAW";
+
+const TITLE_WIDTH: u32 = 200;
+const TITLE_HEIGHT: u32 = 100;
 
 pub fn show_game_over(gs: &mut GameState, gf: &GameFont, canvas: &mut Canvas<Window>) -> bool {
     canvas.set_draw_color(Color::BLACK);
@@ -25,7 +32,21 @@ pub fn show_game_over(gs: &mut GameState, gf: &GameFont, canvas: &mut Canvas<Win
         .load_font(poetsen_font_path, 24)
         .unwrap_or_else(|_| panic!("Failed to load font {}", poetsen_font_path.display()));
 
-
+    let title_str: &str = 
+        match gs.score_p1.cmp(&gs.score_p2) {
+            Ordering::Less => { DEFEAT_TITLE },
+            Ordering::Equal => { DRAW_TITLE },
+            Ordering::Greater => { VICTORY_TITLE },
+        };
+    
+    let rect_title = Rect::new(
+        (WINDOW_WIDTH / 2 - TITLE_WIDTH / 2) as i32,
+        0,
+        TITLE_WIDTH,
+        TITLE_HEIGHT,
+    );
+    
+    loop {}
     true
 }
 
