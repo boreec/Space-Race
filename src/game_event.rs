b6643907ce929371db462b3210c75629c8840dca
Cyprien Borée <boreec@tuta.io>
2022-10-27 18:06:09 +0900

    move FrameEvent struct to game_event.rs

--- src/game_event.rs
+++ src/game_event.rs
@@ -8,6 +8,8 @@ use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::EventPump;
 
+pub struct FrameEvent;
+
 pub fn handle_game_events(
     gs: &mut GameState,
     event_pump: &mut EventPump,
