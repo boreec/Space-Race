d47bdd36560acc2582ef9f58d726ef651e6c6a2e
Cyprien Borée <boreec@tuta.io>
2022-10-27 18:01:08 +0900

    move handle_game_events to its own file

---
+++ src/game_event.rs
@@ -0,0 +1,78 @@
+use crate::update_cpu;
+use crate::GameFont;
+use crate::GameSFX;
+use crate::GameState;
+use crate::view::*;
+
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::EventPump;
+
+pub fn handle_game_events(
+    gs: &mut GameState,
+    event_pump: &mut EventPump,
+    sounds: &GameSFX,
+    canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
+    gf: &GameFont
+) {
+    let event = event_pump.wait_event();
+
+    if event.is_user_event() {
+        // update missile coordinates here
+        for m in &mut gs.missiles {
+            m.update();
+        }
+
+        if gs.spaceship_p1.is_alive && gs.collision_occurred_for(&gs.spaceship_p1) {
+            gs.spaceship_p1.die();
+            sounds.play_collision();
+        }
+        if gs.spaceship_p2.is_alive && gs.collision_occurred_for(&gs.spaceship_p2) {
+            gs.spaceship_p2.die();
+            sounds.play_collision();
+        }
+        update_cpu(gs, sounds);
+        if !gs.spaceship_p1.is_alive && gs.spaceship_p1.can_respawn() {
+            gs.reset_spaceship_p1();
+        }
+        draw_game(canvas, gs, gf);
+    } else {
+        match event {
+            Event::Quit { .. }
+            | Event::KeyDown {
+                keycode: Some(Keycode::Escape),
+                ..
+            } => {
+                gs.is_game_over = true;
+            }
+            Event::KeyDown {
+                keycode: Some(Keycode::Up),
+                ..
+            } => {
+                if gs.spaceship_p1.is_alive {
+                    gs.spaceship_p1.move_upward();
+                    if GameState::has_spaceship_scored(&gs.spaceship_p1) {
+                        sounds.play_score();
+                        gs.score_p1 += 1;
+                        gs.reset_spaceship_p1();
+                    }
+                }
+            }
+            Event::KeyDown {
+                keycode: Some(Keycode::Down),
+                ..
+            } => {
+                if gs.spaceship_p1.is_alive && gs.spaceship_p1.can_move_downward() {
+                    gs.spaceship_p1.move_downward();
+                }
+            }
+            Event::KeyDown {
+                keycode: Some(Keycode::Space),
+                ..
+            } => {
+                gs.is_game_restarted = true;
+            }
+            _ => {}
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,19 @@
 extern crate sdl2;
 
 use crate::disclaimer::*;
+use crate::game_event::*;
 use crate::game_font::GameFont;
 use crate::game_over::*;
 use crate::game_sfx::GameSFX;
 use crate::game_state::GameState;
-use crate::view::*;
 
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
-use sdl2::EventPump;
 
 use std::time::Duration;
 
 mod disclaimer;
+mod game_event;
 mod game_font;
 mod game_over;
 mod game_sfx;
@@ -87,74 +85,7 @@ fn run_game(
     }
 }
 
-fn handle_game_events(
-    gs: &mut GameState,
-    event_pump: &mut EventPump,
-    sounds: &GameSFX,
-    canvas: &mut sdl2::render::Canvas<sdl2::video::Window>,
-    gf: &GameFont
-) {
-    let event = event_pump.wait_event();
 
-    if event.is_user_event() {
-        // update missile coordinates here
-        for m in &mut gs.missiles {
-            m.update();
-        }
-
-        if gs.spaceship_p1.is_alive && gs.collision_occurred_for(&gs.spaceship_p1) {
-            gs.spaceship_p1.die();
-            sounds.play_collision();
-        }
-        if gs.spaceship_p2.is_alive && gs.collision_occurred_for(&gs.spaceship_p2) {
-            gs.spaceship_p2.die();
-            sounds.play_collision();
-        }
-        update_cpu(gs, sounds);
-        if !gs.spaceship_p1.is_alive && gs.spaceship_p1.can_respawn() {
-            gs.reset_spaceship_p1();
-        }
-        draw_game(canvas, gs, gf);
-    } else {
-        match event {
-            Event::Quit { .. }
-            | Event::KeyDown {
-                keycode: Some(Keycode::Escape),
-                ..
-            } => {
-                gs.is_game_over = true;
-            }
-            Event::KeyDown {
-                keycode: Some(Keycode::Up),
-                ..
-            } => {
-                if gs.spaceship_p1.is_alive {
-                    gs.spaceship_p1.move_upward();
-                    if GameState::has_spaceship_scored(&gs.spaceship_p1) {
-                        sounds.play_score();
-                        gs.score_p1 += 1;
-                        gs.reset_spaceship_p1();
-                    }
-                }
-            }
-            Event::KeyDown {
-                keycode: Some(Keycode::Down),
-                ..
-            } => {
-                if gs.spaceship_p1.is_alive && gs.spaceship_p1.can_move_downward() {
-                    gs.spaceship_p1.move_downward();
-                }
-            }
-            Event::KeyDown {
-                keycode: Some(Keycode::Space),
-                ..
-            } => {
-                gs.is_game_restarted = true;
-            }
-            _ => {}
-        }
-    }
-}
 
 fn update_cpu(gs: &mut GameState, sounds: &GameSFX) {
     if !gs.spaceship_p2.is_alive {
