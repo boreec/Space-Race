369293b2ab95c1c0026f9c6073647f6506fbc1bc
Cyprien Borée <boreec@tuta.io>
2022-10-27 15:15:00 +0900

    add line to replay on game over screen

--- src/game_over.rs
+++ src/game_over.rs
@@ -21,6 +21,8 @@ const DRAW_TITLE: &str= "DRAW";
 
 const TITLE_WIDTH: u32 = 200;
 const TITLE_HEIGHT: u32 = 100;
+const REPLAY_WIDTH: u32 = 600;
+const REPLAY_HEIGHT: u32 = 30;
 
 const SCREEN_PADDING: i32 = 20;
 const SCREEN_DURATION: u64 = 10;
@@ -49,9 +51,13 @@ pub fn show_game_over(gs: &mut GameState, gf: &GameFont, canvas: &mut Canvas<Win
             Ordering::Greater => { "You won! You have nothing to prove anymore!"},        
         };
     
+    let replay_str: &str = "Press [space] key to replay now, or the game will end!";
+    
     let surface_title = gf.surface_from_str(title_str, &big_font, Color::WHITE);
     
     let surface_message = gf.surface_from_str(message_str, &small_font, Color::WHITE);
+    
+    let surface_replay = gf.surface_from_str(replay_str, &small_font, Color::WHITE);
     
     let rect_title = Rect::new(
         (WINDOW_WIDTH / 2 - TITLE_WIDTH / 2) as i32,
@@ -63,9 +69,16 @@ pub fn show_game_over(gs: &mut GameState, gf: &GameFont, canvas: &mut Canvas<Win
     let rect_message = Rect::new(
         SCREEN_PADDING,
         (WINDOW_HEIGHT / 2) as i32,
-        WINDOW_WIDTH - SCREEN_PADDING as u32,
+        WINDOW_WIDTH - 2 * SCREEN_PADDING as u32,
         30
     );
+    
+    let rect_replay = Rect::new(
+        (WINDOW_WIDTH / 2 - REPLAY_WIDTH / 2) as i32,
+        (WINDOW_HEIGHT / 2) as i32 + SCREEN_PADDING + 30,
+        REPLAY_WIDTH,
+        REPLAY_HEIGHT
+    ); 
     
     let texture_title = texture_creator
         .create_texture_from_surface(&surface_title)
@@ -75,12 +88,19 @@ pub fn show_game_over(gs: &mut GameState, gf: &GameFont, canvas: &mut Canvas<Win
         .create_texture_from_surface(&surface_message)
         .expect("Failed to create texture for Game Over's main message!");
     
+    let texture_replay = texture_creator
+        .create_texture_from_surface(&surface_replay)
+        .expect("Failed to write replay's line in Game Over's screen!");
+    
     canvas
         .copy(&texture_title, None, rect_title)
         .expect("Failed to copy Game Over's Title's texture to canvas!");
     canvas
         .copy(&texture_message, None, rect_message)
         .expect("Failed to copy Game Over's message's texture to canvas!");
+    canvas
+        .copy(&texture_replay, None, rect_replay)
+        .expect("Failed to copy Game Over's replay's texture to canvas!");
     
     canvas.present();
     
